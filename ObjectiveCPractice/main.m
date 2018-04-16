91209ebc2e88adb71a7d47ddf6b2f6c8e590b2f6
Kanata <thunderpooh3@gmail.com>
2018-04-16 14:00:52 -0700

    Initial Commit

---
+++ ObjectiveCPractice/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveCPractice
+//
+//  Created by Kanata Shimomoto on 2018-04-16.
+//  Copyright © 2018 Kanata Shimomoto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
